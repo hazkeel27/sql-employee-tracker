d6de0d6f76e107a8289281f9f56f59a59652b5f3
hazkeel <hazkeel27@gmail.com>
2023-08-17 04:59:22 -0400

    created schema.sql

---
+++ README.md
@@ -0,0 +1,14 @@
+Create a class for the entire database
+
+Create child classes for department, role, and employee which includes methods for:
+- viewing data
+- adding data
+- deleting data
+
+Create a separate file which returns inquirer questions
+
+Create a main index.js file which includes a while loop and incorporates inquirer file and child classes
+
+create schema.sql and seeds.sql
+
+Create test files for all js files
---
+++ database/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABaSE IF EXISTS company_db;
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+    dep_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dep_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_title VARCHAR(30) NOT NULL,
+    role_salary DECIMAL(10, 2) NOT NULL,
+    dep_id INT NOT NULL,
+    FOREIGN KEY (dep_id) REFERENCES department(dep_id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    emp_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(role_id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(emp_id) ON DELETE SET NULL
+);
